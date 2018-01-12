5267158f91d81878846522c714f893177226f791
Jeremy Day <jadaytime@gmail.com>
2018-01-11 20:47:56 -0500

    fix vector preparation to correctly sort vector

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,34 @@
 fn main() {
 
-    let mut colors = vec![vec![25u8, 12u8, 10u8]];
+    let mut colors = vec![
+        vec![128u8, 128u8, 128u8],
+        vec![150u8, 250u8, 250u8],
+        vec![150u8, 150u8, 150u8],
+        vec![16u8, 32u8, 48u8],
+        vec![64u8, 33u8, 0u8],
+        vec![200u8, 200u8, 200u8],
+        vec![64u8, 31u8, 0u8],
+    ];
 
-    prepare_array(&mut colors, 3);
+    prepare_vec(&mut colors[..], 3);
+    println!("{:?}", colors);
 }
 
-fn prepare_array(mut vec: &mut Vec<Vec<u8>>, dimension: u8) {
-
-    fn sort_split(mut v: &mut [Vec<u8>], current_dim: u8, max_dim: u8) {
+fn prepare_vec(mut vec: &mut [Vec<u8>], dimension: u8) {
+    fn sort_split(v: &mut [Vec<u8>], current_dim: u8, max_dim: u8) {
         if v.len() == 1 {
-            ()
+            return; 
         }
 
+        // We are sorting all the way here, but all we really need to do is
+        // find the median element and partition around it, like in quicksort.
+        // This could be faster, but for now this is plenty fast enough as we
+        // only pay this cost when constructing the tree and not when querying.
         v.sort_unstable_by_key(|k| k[current_dim as usize]);
-        let mut left = &v[..v.len()/2];
-        let mut right = &v[v.len()/2..];
-        //let (left, right)  = v.split_at((v.len() / 2) as usize);
-        //println!("left: {:?}\nright: {:?}", left, right);
+        let middle = v.len() / 2;
         let next_dim = (current_dim + 1) % max_dim;
-        sort_split(&mut &left, next_dim, max_dim);
-        sort_split(&mut &right, next_dim, max_dim);
+        sort_split(&mut v[..middle], next_dim, max_dim);
+        sort_split(&mut v[middle..], next_dim, max_dim);
     }
-
-    println!("this function was called");
-    vec.push(vec![1u8, 1u8, 1u8]);
-    vec.push(vec![1u8, 1u8, 1u8]);
-    vec.push(vec![1u8, 1u8, 1u8]);
-    vec.push(vec![1u8, 1u8, 1u8]);
     sort_split(&mut vec[..], 0, dimension - 1);
 }
